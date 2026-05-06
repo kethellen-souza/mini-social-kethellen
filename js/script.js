c64ff6285c2b324780ba16366ebfa284e1b8216b
kethellen-souza <kethellen.7595145@aluno.mg.gov.br>
2026-05-06 16:02:06 +0000

    dia 06/05

--- js/script.js
+++ js/script.js
@@ -1,63 +1,87 @@
-//=== ESTADO (dados da aplicação) === 
+//=== BANCO DE DADOS (JSON simulado) === 
 
-let likeCount = 0;
-let dislikeCount = 0;
-let curtido = false;
-let descurtido = false;
+let post = {
 
-//=== SERVICE (regras de negocio)
+likeCount: 0,
+dislikeCount: 0,
+curtido: false,
+descurtido: false
+}
+
+
+//=== SERVICE (regras de negócio) === 
+
 function curtir() {
-  if (curtido == false){
-    likeCount++;
-    curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
+  if (post.curtido == false){
+    post.likeCount++;
+    post.curtido = true;
 
-    if(descurtido == true){
-      dislikeCount--;
-      descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
+    if(post.descurtido == true){
+      post.dislikeCount--;
+     post.descurtido = false;
     }
 
   }else{
-    likeCount--;
-    curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
+    post.likeCount--;
+    post.curtido = false;
   }
 
 }
 
 function descurtir() {
-  if(descurtido == false){
-    dislikeCount++;
-    descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+  if(post.descurtido == false){
+    post.dislikeCount++;
+    post.descurtido = true;
 
-    if(curtido == true){
-      likeCount--;
-      curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
+    if(post.curtido == true){
+      post.likeCount--;
+      post.curtido = false;
     }
 
   }
   else{
-    dislikeCount--;
-    descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+    post.dislikeCount--;
+    post.descurtido = false;
   }
 }
-//===CONTROLLER (intermediação)===
+
+//=== API SIMULADA ===
+
+function getpost(){
+return post;
+}
+function likepost(){
+  curtir();
+return post;
+}
+function dislikepost(){
+  descurtir();
+return post;
+
+}
+
+// === VIEW (interface/renderização)===
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação)===
 
 function clicarCurtir(){
-curtir();
+  curtir();
+  atualizarTela();
 }
-function clicarDescurtir (){
+function clicarDescurtir(){
   descurtir();
+  atualizarTela();
 }
 
+// === EVENTOS ===
 
-
-
-//=== EVENTOS === 
 document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
 document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO ===  
+
+atualizarTela();
